b4bba14afd39aa11df76a35d365b0168a507cb66
elvinagamayanti <pinaaae@gmail.com>
2024-08-15 15:09:40 +0700

    detail mitra error

--- resources/views/mitradetail.php
+++ resources/views/mitradetail.php
@@ -9,7 +9,7 @@
     <dl class="divide-y divide-gray-100 dark:divide-gray-700">
       <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
         <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-gray-100">Nama Mitra</dt>
-        <dd class="mt-1 text-sm leading-6 text-gray-700 dark:text-gray-300 sm:col-span-2 sm:mt-0">{{ $mira['name'] }}</dd>
+        <dd class="mt-1 text-sm leading-6 text-gray-700 dark:text-gray-300 sm:col-span-2 sm:mt-0">{{ $mitra['name'] }}</dd>
       </div>
       <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
         <dt class="text-sm font-medium leading-6 text-gray-900 dark:text-gray-100">ID Sobat</dt>
